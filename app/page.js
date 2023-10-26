0c4da7fa199f603999b996adf5b13e61a1855b1a
Marc Lou <marc.louvion@gmail.com>
2023-10-26 11:15:25 +0800

    (new) default templates come with signup/user-account buttons

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+import ButtonSignin from "@/components/ButtonSignin";
+
+export default function Page() {
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
