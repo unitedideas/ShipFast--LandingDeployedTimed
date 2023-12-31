77923632704d0df6ba981a2461c384e8a7eb8868
Shane Cheek <shane.cheek@agilebits.com>
2023-12-30 19:57:51 -0800

    test

--- app/api/auth/callback/route.js
+++ app/api/auth/callback/route.js
@@ -8,18 +8,16 @@ export const dynamic = "force-dynamic";
 // This route is called after a successful login. It exchanges the code for a session and redirects to the callback URL (see config.js).
 export async function GET(req) {
   const requestUrl = new URL(req.url);
+  console.log("requestUrl", requestUrl);
   const code = requestUrl.searchParams.get("code");
+    console.log("code", code);
 
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
+    console.log("supabase", supabase);
     await supabase.auth.exchangeCodeForSession(code);
   }
 
   // URL to redirect to after sign in process completes
-
-  console.log(requestUrl)
-  console.log(requestUrl.origin)
-  console.log(config.auth)
-  console.log(config.auth.callbackUrl)
   return NextResponse.redirect(requestUrl.origin + config.auth.callbackUrl);
 }
