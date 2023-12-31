dd59ffec0501cdc6525c4b6fc80c6c1c7e000d70
Shane Cheek <shane.cheek@agilebits.com>
2023-12-30 16:31:16 -0800

    patch

--- app/page.js
+++ app/page.js
@@ -6,12 +6,14 @@ import Pricing from "@/components/Pricing";
 import FAQ from "@/components/FAQ";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
+import ButtonLead from "@/components/ButtonLead";
 
 export default function Home() {
     return (
         <>
             <Header />
             <main>
+                <ButtonLead extraStyle="!max-w-none !w-full"/>
                 <Hero />
                 <Problem />
                 <FeaturesAccordion />
