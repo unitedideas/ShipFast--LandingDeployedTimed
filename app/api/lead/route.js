5848d3014bcc1a8153c050929e0b0d25c689b00b
Shane Cheek <shane.cheek@agilebits.com>
2023-12-30 17:46:24 -0800

    🤔

--- app/api/lead/route.js
+++ app/api/lead/route.js
@@ -5,10 +5,9 @@ import { cookies } from "next/headers";
 // This route is used to store the leads that are generated from the landing page.
 // The API call is initiated by <ButtonLead /> component
 export async function POST(req) {
-  const body = await req.json();
-
   console.log(`api/lead-body: ${body}`);
   console.log(`api/lead-body.email ${body.email}`);
+  const body = await req.json();
 
   if (!body.email) {
     return NextResponse.json({ error: "Email is required" }, { status: 400 });
