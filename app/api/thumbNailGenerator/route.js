4ea81e6742c46b77f336f2183dcef5e74412eb9f
Shane Cheek <shane.cheek@agilebits.com>
2024-01-01 13:05:30 -0800

    update

--- app/api/thumbNailGenerator/route.js
+++ app/api/thumbNailGenerator/route.js
@@ -7,27 +7,25 @@ const openAI = new OpenAI();
 export async function POST(req) {
     await console.log("in the post call");
     const body = await req.json();
-    await console.log("body", body);
 
-    if (!body.sceneDescription) {
-        console.log("Description missing");
-        return NextResponse.json({error: "Description missing"}, {status: 400});
+    const genBody = {
+        model: "dall-e-3",
+        prompt: `In the form of: ${body.artStyle}.
+        Scene: ${body.sceneDescription}.
+        Main subject: ${body.subject}.
+        The mood is ${body.tone}.
+        Make sure you dont modify the following text: "${body.tnText}".
+        This image needs to maximize views based on the data of views for other youtube thumbnails.
+        Do no include the YouTube logo or the text "YouTube"`,
+        quality: "hd",
+        size: "1792x1024",
+        style: "vivid",
+        response_format: "b64_json",
     }
 
-    console.log("inside generateYoutubeThumbnail");
     try {
-        const genBody = {
-            model: "dall-e-3",
-            prompt: `In the form of: ${body.artStyle}. Scene: ${body.sceneDescription}. Main subject: ${body.subject}. The mood is ${body.tone}. Make sure you dont modify the following text: "${body.tnText}". This image needs to maximize views based on the data of views for other youtube thumbnails. Do no include the YouTube logo or the text "YouTube"`,
-            quality: "hd",
-            size: "1792x1024",
-            style: "vivid",
-            response_format: "b64_json",
-        }
-
         const thumbNail = await openAI.images.generate(genBody);
-        console.log("thumbnail", thumbNail);
-        // Return a NextResponse with the thumbnail data
+
         return NextResponse.json({ thumbnail: thumbNail }, { status: 200 });
     } catch (e) {
         console.error("GPT Error: " + e);
@@ -35,4 +33,3 @@ export async function POST(req) {
         return NextResponse.json({ error: "An internal server error occurred." }, { status: 500 });
     }
 }
-
