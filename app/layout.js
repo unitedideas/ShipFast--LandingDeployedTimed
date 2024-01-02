9c7c0dec726fe286c28551d7651564aea750b23a
Shane Cheek <shane.cheek@agilebits.com>
2024-01-02 08:23:38 -0800

    add speed analysis

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
 import {Analytics} from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const font = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <body>
         {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
         <Analytics />
+        <SpeedInsights />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
