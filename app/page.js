b30bb1ce9dcaef8df8f2f5989d0ef507ab6b3042
Shane Cheek <shane.cheek@agilebits.com>
2024-04-15 21:13:39 -0700

    table

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
 import Footer from "@/components/Footer";
-import DataTableDemo from "@/components/table";
+import DataTableDemo from "@/components/Table";
 
 export default function Home() {
     return (
